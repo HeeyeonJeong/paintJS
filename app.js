c8219f641a0847e92c2f58813d8a15b26a3225c6
HeeyeonJeong <junghy14971@gmail.com>
2020-09-22 16:37:02 +0900

    Changing Color

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const canvas = document.querySelector("#jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.querySelectorAll(".jsColor");
 
 canvas.width = 500;
 canvas.height = 500;
 
 let painting = false;
 
-ctx.storkeStyle = "#2c2c2c";
+ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
 function stopPainting() {
@@ -29,9 +30,18 @@ function onMouseMove(event) {
   }
 }
 
+function handleClickColor(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach((color) => {
+  color.addEventListener("click", handleClickColor);
+});
